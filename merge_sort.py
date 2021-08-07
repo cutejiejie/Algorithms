14cb5f6e24c024883e0a3ad344f587d2e623a0c8
pengjie <2267258221@qq.com>
2021-08-07 22:19:49 +0800

    归并排序

--- merge_sort.py
+++ merge_sort.py
@@ -6,7 +6,7 @@ def merge(li, low, mid, high):
         if li[i] < li[j]:
             ltmp.append(li[i])
             i += 1
-        if li[i] > li[j]:
+        else:
             ltmp.append(li[j])
             j += 1
     # while执行完，肯定有一部分没数了
@@ -17,17 +17,30 @@ def merge(li, low, mid, high):
         ltmp.append(li[j])
         j += 1
     li[low:high+1]=ltmp
-    return li
+    # return li
 def merge_sort(li, low, high):
-    mid = (low + high)//2
-    if low<high:
+    if low < high: # 至少有两个元素，递归
+        mid = (low + high) // 2
         merge_sort(li, low, mid)
         merge_sort(li, mid+1, high)
         merge(li, low, mid, high)
     return li
-
-
-
-li=[52,34,68,12,61,83,49,26,57,60]
+#
+li = list(range(16))
+import random
+random.shuffle(li)
+# li=[52,34,68,12,61,83,49,26,57,60]
 print(merge_sort(li, 0, len(li)-1))
+# count = 0
+# def merge_sort_test(li, low, high):
+#     if low < high: # 至少有两个元素，递归
+#         mid = (low + high) // 2
+#         merge_sort_test(li, low, mid)
+#         merge_sort_test(li, mid+1, high)
+#         print(li[low:high+1])
 
+# li = list(range(16))
+# import random
+# random.shuffle(li)
+# print(li)
+# merge_sort(li, 0, len(li)-1)
