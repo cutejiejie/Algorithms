e52b81ce81d0a4c5339bce9a0d62c7b343626d26
pengjie <2267258221@qq.com>
2021-08-11 21:35:23 +0800

    基数排序

--- radix_sort.py
+++ radix_sort.py
@@ -1,3 +1,6 @@
+from cal_time import *
+
+@cal_time
 def radix_sort(li):
     max_num = max(li)  # 最大值 9->1, 99->2, 888->3, 10000->5
     # 迭代多少次  it=0是个位   it=1是十位 it=2是百位
@@ -22,9 +25,40 @@ def radix_sort(li):
         it += 1
 
 
-import random#
-li = [random.randint(0, 100) for _ in range(100)]
+def partition(li, left, right):
+    tmp = li[left]
+    while left < right:
+        while left < right and li[right] >= tmp: #从右面找到比tmp小的数
+            right -= 1 #往左走一步
+        li[left] = li[right] #把右边的值写到左边空位上
+        # print(li, 'right')
+        while left < right and li[left] <= tmp:
+            left += 1 #往左走一步
+        li[right] = li[left] #把左边的值写到右边空位上
+        # print(li, 'left')
+    li[left] = tmp #把tmp归位
+    return left
+def _quick_sort(li, left, right):
+    if left < right: #至少两个元素
+        mid = partition(li, left, right)
+        _quick_sort(li, left, mid-1)
+        _quick_sort(li, mid+1, right)
+@cal_time
+def quick_sort(li):
+    _quick_sort(li, 0, len(li) - 1)
+
+
+import random, copy
+# li = [random.randint(0, 100) for _ in range(100)]
+# random.shuffle(li)
+# print(li)
+# radix_sort(li)
+# print(li)
+# li = list(range(100000))
+li = [random.randint(0, 10000000000) for _ in range(100000)]
 random.shuffle(li)
-print(li)
-radix_sort(li)
-print(li)
+li1 = copy.deepcopy(li)
+li2 = copy.deepcopy(li)
+li3 = copy.deepcopy(li)
+quick_sort(li1) #nlogn logn=log(2,n)
+radix_sort(li2) #kn k=log(10,n)
