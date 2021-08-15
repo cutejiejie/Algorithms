23ee9aff5bb31ac06a72a1e51e36ba0d25094708
pengjie <2267258221@qq.com>
2021-08-15 10:25:53 +0800

    栈的应用——括号匹配问题

--- 数据结构/stack.py
+++ 数据结构/stack.py
@@ -10,10 +10,32 @@ class Stack:
             return self.stack[-1]
         else:
             return None
-stack = Stack()
-stack.push(1)
-stack.push(2)
-stack.push(3)
-stack.push(90)
-print(stack.pop())
-print(stack.get_top())
+    def is_empty(self):
+        return len(self.stack) == 0
+
+def brace_match(s):
+    match = {'}':'{', ']':'[', ')':'('}
+    stack =Stack()
+    for ch in s:
+        if ch in {'{', '[', '('}:
+            stack.push(ch)
+        else:
+            if stack.is_empty():
+                return False
+            elif stack.get_top() == match[ch]:
+                stack.pop()
+            else:
+                return False
+    if stack.is_empty():
+        return True
+    else:
+        return False
+# stack = Stack()
+# stack.push(1)
+# stack.push(2)
+# stack.push(3)
+# stack.push(90)
+# print(stack.pop())
+# print(stack.get_top())
+print(brace_match('{[]{()[]}[]()}'))
+print(brace_match('[){}(]'))
